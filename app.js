0d3903425fde77c8cca94970716befb05a6ed503
Andrew Zey <andrew@andrewzey.com>
2015-02-23 10:23:51 -0800

    complete angular version of todo-list

---
+++ app.js
@@ -0,0 +1,21 @@
+(function() {
+  angular.module('todoApp', [])
+  .controller('todosCtrl', ['$scope', function($scope) {
+    $scope.newTask = null;
+    $scope.addTask = function(taskText) {
+      var newTask = {
+        text: taskText,
+        isComplete: false
+      };
+      $scope.todos.push(newTask)
+      $scope.newTask = null;
+    };
+
+    $scope.todos = [
+      { text: 'Create a React todo list', isComplete: false },
+      { text: 'Create an Angular todo list', isComplete: false },
+      { text: 'Show Daniel how unit testing works', isComplete: false },
+    ]
+  }]);
+
+})();
